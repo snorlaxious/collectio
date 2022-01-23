251631fc6a7b143540ef1c04c2ae75fe99bb8c50
Junghyun Park <qwertyazsxc@gmail.com>
2022-01-23 22:25:18 +0900

    backend init

---
+++ back/src/main/java/snorlaxious/collectio/CollectioApplication.java
@@ -0,0 +1,13 @@
+package snorlaxious.collectio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectioApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/snorlaxious/collectio/CollectioApplicationTests.java
@@ -0,0 +1,13 @@
+package snorlaxious.collectio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
